fbad876e651a250d53bd66532cc4b05815ffe070
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-09-08 22:59:03 +0300

    added two radioBtns

--- main/lib/screens/secondscreen.dart
+++ main/lib/screens/secondscreen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, non_constant_identifier_names, sort_child_properties_last, prefer_const_literals_to_create_immutables
 
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:main/MyDropDownItems.dart';
@@ -14,6 +16,8 @@ class SecondScreen extends StatefulWidget {
 }
 
 class _SecondScreenState extends State<SecondScreen> {
+  String radioGroup_Class = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,13 +27,13 @@ class _SecondScreenState extends State<SecondScreen> {
         centerTitle: true,
       ),
       body: StructureWidget.formatPageWidget_Standard(
-        networkImage: secondScreenNetowrkImage(),
+        networkImage: screen_Two_NetowrkImage(),
         childWidget: body(),
       ),
     );
   }
 
-  NetworkImage secondScreenNetowrkImage() {
+  NetworkImage screen_Two_NetowrkImage() {
     return NetworkImage(
         "https://images.pexels.com/photos/3616764/pexels-photo-3616764.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
     //"https://images.pexels.com/photos/3258236/pexels-photo-3258236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
@@ -44,8 +48,20 @@ class _SecondScreenState extends State<SecondScreen> {
           question: "How many person?",
           myItems: ["1-9", "10-19", "20-29", "30-39"],
         ),
-        Divider(color: Colors.tealAccent, thickness: 3),
+        screen_Two_DefaultDivider(),
         enterClass_Msg(),
+        radioListTile(
+          value: "business",
+          title: "Business",
+          subTile: "Hot! Only 200\$ per Person",
+        ),
+        radioListTile(
+          value: "economy",
+          title: "Economy",
+          subTile: "Hot! 150\$ per Person",
+          icon: Icon(Icons.money_off_csred),
+        ),
+        screen_Two_DefaultDivider(),
       ],
     );
   }
@@ -80,10 +96,34 @@ class _SecondScreenState extends State<SecondScreen> {
     );
   }
 
+  Divider screen_Two_DefaultDivider() {
+    return Divider(color: Colors.tealAccent, thickness: 3);
+  }
+
   Container enterClass_Msg() {
     return askingInput_Msg(
       text: "Please Select your Class!",
       foregroundColor: Colors.pink,
+    );
+  }
+
+  RadioListTile<String> radioListTile({
+    required String value,
+    required String title,
+    required String subTile,
+    Icon icon = const Icon(Icons.attach_money),
+  }) {
+    return RadioListTile(
+      value: value,
+      groupValue: radioGroup_Class,
+      title: Text(title),
+      subtitle: Text(subTile),
+      secondary: icon,
+      onChanged: (value) {
+        setState(() {
+          radioGroup_Class = value!.toString();
+        });
+      },
     );
   }
 }
